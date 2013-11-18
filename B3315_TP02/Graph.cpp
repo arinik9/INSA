2669d3c70a6a523b5f0f783d60e0bdd533411b03
Nejat <arinik9@hotmail.com>
2013-11-19 00:40:21 +0100

    Update Graph.cpp
    insert metodunda karsilasilan hatayi [] operatoruyle duzeltme

--- B3315_TP02/Graph.cpp
+++ B3315_TP02/Graph.cpp
@@ -7,10 +7,10 @@ void Graph::Ajouter(string aQuelHeure,string cible_url,string extension,string s
 	int hour = atoi(s.c_str());
 				if(analyse.find(source_url) == analyse.end())
 				{
-					nbHit_Horaires a;
+					nbHit_Horaires a={{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},""};
 					a.extension=extension;
 					a.heures[hour-1]++;
-					analyse.insert(make_pair(source_url,make_pair(cible_url,a)));
+					analyse[source_url][cible_url]=a;
 				}
 			else{
 					if (analyse.find(source_url)->second.find(cible_url) == analyse.find(source_url)->second.end())
@@ -18,7 +18,7 @@ void Graph::Ajouter(string aQuelHeure,string cible_url,string extension,string s
 						nbHit_Horaires a;
 						a.extension=extension;
 						a.heures[hour-1]++;
-						analyse.find(source_url)->second.insert(make_pair(cible_url,a));
+						analyse[source_url][cible_url]=a;
 					}
 					else;
 				}
