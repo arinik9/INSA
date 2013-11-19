38d9dc5415dcf9b980f63b44b3d1eae1c244c7e4
Nejat <arinik9@hotmail.com>
2013-11-20 00:03:59 +0100

    Update LectureEcriture.cpp
    19.11.2013 C++ TP2'sinde yaptiklarimiz

--- B3315_TP02/LectureEcriture.cpp
+++ B3315_TP02/LectureEcriture.cpp
@@ -1,47 +1,30 @@
-#include "LectureEcriture.h"
+#ifndef LECTUREECRITURE_H_
+#define LECTUREECRITURE_H_
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
 
-UnLog LectureEcriture::ProchainLog()
+using namespace std;
+
+struct UnLog
 {
-		UnLog logEnCours;
-		string s="",c="";
+	string extension;
+	string cible_url; 
+	string source_url;
+	string aQuelHeure;
+};
 
-		getline(myFile,s,':');
-		getline(myFile,logEnCours.aQuelHeure,':');
+class LectureEcriture {
+private:
+	ifstream myFile;
+public:
 
-		getline(myFile,s,'"');
-		getline(myFile,s,' ');
+	UnLog ProchainLog();
+	LectureEcriture(string nomfic);
+	bool EstFini();
+	virtual ~LectureEcriture();
 
-		getline(myFile,logEnCours.cible_url,' ');  // f="html" mesela
-		  unsigned found=logEnCours.cible_url.find_last_of(".");
-			c=logEnCours.cible_url.substr(found+1);
-			  if (logEnCours.cible_url.find("?") !=std::string::npos){
-					c=c.substr(0,3);
-					  }
+};
 
-		transform(c.begin(),c.end(),c.begin(),::tolower);
-		logEnCours.extension=c;
-		//cout << logEnCours.extension <<endl;,
-
-		getline(myFile,s,'"');
-		getline(myFile,s,'"');
-		getline(myFile,logEnCours.source_url,'"');
-
-		getline(myFile,s,'\n');
-		return logEnCours;
-}
-
-
-
-
-bool LectureEcriture:: EstFini()
-{
-return(myFile.eof());
-}
-
-LectureEcriture::LectureEcriture(string nomfic):myFile(nomfic.c_str()) {
-	// TODO Auto-generated constructor stub
-}
-
-LectureEcriture::~LectureEcriture() {
-	// TODO Auto-generated destructor stub
-}
+#endif /* LECTUREECRITURE_H_ */
