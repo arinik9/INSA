608e834d1b6d28a44ff2013a2778326667a961a3
Nejat <arinik9@hotmail.com>
2013-11-20 00:01:59 +0100

    Update main.cpp
    19.11.2013 C++ TP2'sinde yaptiklarimiz

--- B3315_TP02/main.cpp
+++ B3315_TP02/main.cpp
@@ -1,9 +1,16 @@
 #include "LectureEcriture.h"
 #include "Graph.h"
+#include <string>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <stdlib.h>
 
 using namespace std;
 
-int main(int argc,char **lstarg)
+int main(int argc, char *argv[])
 {
 	Graph g;
 	LectureEcriture l("anonyme.log");
@@ -11,9 +18,16 @@ int main(int argc,char **lstarg)
 	{
 		UnLog ul;
 		ul=l.ProchainLog();
+		cout<<"AJOUT"<<endl;
 		g.Ajouter(ul.aQuelHeure,ul.cible_url,ul.extension,ul.source_url);
 	}
+	
+	cout<<"ARG-C "<<argc<<endl;
+	copy(argv,argv+argc-1,ostream_iterator<char *>(cout,";"));
+	if (argc == 1)
+			    {
+				  printf("argv = %s\n" ,argv[0]);
+			    }
+		  return 0;
 
-
-	return 0;
 }
