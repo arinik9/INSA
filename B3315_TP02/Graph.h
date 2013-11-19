b9adde7283bf2006cb78e0726d4773c3f3a3a547
Nejat <arinik9@hotmail.com>
2013-11-19 23:58:12 +0100

    Update Graph.h
    19.11.2013 C++ TP2'sinde yaptiklarimiz

--- B3315_TP02/Graph.h
+++ B3315_TP02/Graph.h
@@ -13,18 +13,25 @@ struct nbHit_Horaires
 {
 	int heures[24];
 	string extension;
+	//vector<int> heures;
+	//nbHit_Horaires():heures(24,0),extenstion(){}
+	nbHit_Horaires & operator = (const nbHit_Horaires &);
+
 };
 
 
+//nbHit_Horaires InitialisationNbHit_Horaires();
 
 class Graph {
 private:
-		typedef map<string, nbHit_Horaires> Cible;
-		typedef map<string, Cible> Referer;
-		Referer analyse;
+		typedef map<string, nbHit_Horaires> Referer;
+		typedef map<string, Referer> Cible;
+		Cible analyse;
+
 public:
+	Graph();
 	virtual ~Graph();
-	//friend ostream & operator << (ostream & os,const Graph & g);
+	friend ostream & operator << (ostream & os,const Graph & g);
 	void Ajouter(string aQuelHeure,string cible_url,string extension,string source_url);
 };
 
